ddacf4328f250a1e86146c77aeae4c81f54c09b3
MGlauer <martinglauer89@gmail.com>
2020-11-25 10:21:59 +0100

    Add interface for OWL parser

---
+++ src/gavel_owl/dialects/owl/parser.py
@@ -0,0 +1,10 @@
+from typing import Iterable
+
+from gavel.dialects.base import parser
+from gavel.dialects.base.parser import Parseable, Target
+
+
+class OWLParser(parser.StringBasedParser):
+
+    def parse(self, structure: Parseable, *args, **kwargs) -> Iterable[Target]:
+        pass
