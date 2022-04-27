850e386c2bf9798fc7475bc5917475381e727d07
MGlauer <martinglauer89@gmail.com>
2022-04-27 18:04:52 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='gavel-owl',
-    version='0.0.2',
+    version='0.0.3.dev0',
     description='An extension for gavel introducing OWL translations.',
     long_description='%s\n%s' % (
         re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
